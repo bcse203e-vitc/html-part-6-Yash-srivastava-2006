83aaf0fa431cd3bddbfa6150d34eb45fe6a42091
Yash-srivastava-2006 <srivastava25012006yash@gmail.com>
2025-02-04 15:36:58 +0530

    Create Q12.js

---
+++ Assignment_6/Q12.js
@@ -0,0 +1,9 @@
+function removeDuplicates(arr) {
+    let uniqueArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (uniqueArray.indexOf(arr[i]) === -1) {
+            uniqueArray.push(arr[i]);
+        }
+    }
+    return uniqueArray;
+}
